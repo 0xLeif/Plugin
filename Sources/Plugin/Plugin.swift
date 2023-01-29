91311a2ad805481727c97dbd06bc44e19a062e93
Leif <zmeriksen@icloud.com>
2023-01-28 17:10:28 -0700

    Add initial documentation

--- Sources/Plugin/Plugin.swift
+++ Sources/Plugin/Plugin.swift
@@ -1,10 +1,23 @@
+/// A protocol that defines a plugin that can be registered and handled by a `Pluginable` object.
 public protocol Plugin {
+    /// The source object that the plugin will modify.
     associatedtype Source
+
+    /// The input value that the plugin will handle.
     associatedtype Input
+
+    /// The value that the plugin will modify from the source object.
     associatedtype Output
 
+    /// The key path to the property that the plugin will modify on the `Source` object.
     var keyPath: WritableKeyPath<Source, Output> { get set }
 
+    /// Handles the input value and modifies the output value at the specified key path on the source object.
+    ///
+    /// - Parameters:
+    ///   - value: The input value that the plugin will handle.
+    ///   - output: The value that the plugin will modify from the source object. It is passed as an inout parameter so that it can be modified directly.
+    /// - Throws: Any errors that occur during handling of the input value.
     func handle(value: Input, output: inout Output) async throws
 }
 
