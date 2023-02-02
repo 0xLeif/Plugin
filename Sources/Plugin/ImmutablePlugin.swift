213d9984e2a2b5fc173fa8fb63fbb38c937781f3
Leif <zmeriksen@icloud.com>
2023-02-01 17:49:33 -0700

    Add missing immutable keyPath

--- Sources/Plugin/ImmutablePlugin.swift
+++ Sources/Plugin/ImmutablePlugin.swift
@@ -8,6 +8,12 @@ public protocol ImmutablePlugin: Plugin where Output == Void {
 }
 
 extension ImmutablePlugin {
+    /// A keypath that provides access to the `immutable` property of the `Source`.
+    public var keyPath: WritableKeyPath<Source, ()> {
+        get { \.immutable }
+        set { }
+    }
+
     public func handle(value: Input, output: inout ()) async throws {
         try await handle(value: value)
     }
