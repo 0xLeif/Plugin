bd5449fc40720589881bacb4ed7373ddfcb2d214
Leif <zmeriksen@icloud.com>
2023-03-05 15:33:11 -0700

    Promote public to open

--- Sources/Plugin/ImmutablePluginable.swift
+++ Sources/Plugin/ImmutablePluginable.swift
@@ -1,6 +1,6 @@
-public class ImmutablePluginable: Pluginable {
+open class ImmutablePluginable: Pluginable {
     /// An array that contains the plugins that have been registered to the object.
-    public var plugins: [any Plugin]
+    open var plugins: [any Plugin]
 
     public init(plugins: [any Plugin] = []) {
         self.plugins = plugins
